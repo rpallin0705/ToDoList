8dae415a81f80522fb9c6c164efb5f5453d84785
rpallin0705 <rpallin0705@g.educaand.es>
2024-01-08 18:43:04 +0100

    Configuradas dependencias desarrollo: dotenv

--- todo-back/src/config.js
+++ todo-back/src/config.js
@@ -1,3 +1,6 @@
+// Se importa dotenv para gestionar .env
+require('dotenv').config();
+
 module.exports = {
     app: {
         port: process.env.PORT || 3000,
--- todo-back/src/index.js
+++ todo-back/src/index.js
@@ -3,6 +3,6 @@ const app = require('./app.js');
 
 // Iniciamos el servidor
 app.listen(app.get('port'), () => {
-    console.log("Servidor escuchando en puerto ", app.get("port") )
+    console.log("Servidor escuchando en puerto ", app.get("port"))
     console.log(`http://localhost:${app.get("port")}`);
 })
